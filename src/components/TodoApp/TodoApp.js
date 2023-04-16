7015788b47eabd67ba084e6d6d7eed26647bc001
Maksymov-Serhii <maksymov.serhii.10.01@gmail.com>
2023-04-17 01:40:53 +0300

    added TodoApp component"

---
+++ src/components/TodoApp/TodoApp.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import AddTask from '../AddTask/AddTask';
+import Header from '../Header/Header';
+import TodoList from '../TodoList/TodoList';
+import styles from './TodoApp.module.css';
+
+class TodoApp extends React.Component {
+
+  constructor() {
+    super();
+    this.handleAddListItem = this.handleAddListItem.bind(this);
+    this.handleDeleteListItem = this.handleDeleteListItem.bind(this);
+  }
+
+  state = {
+    list: [],
+  }
+
+  handleAddListItem(item) {
+    const updatedList = [...this.state.list, item];
+    this.setState({
+      list: updatedList
+    })
+  }
+
+  handleDeleteListItem(id) {
+    const updatedList = this.state.list.filter(item => item.id !== id);
+    this.setState({list: updatedList})
+  }
+  
+  render() {
+    return (
+    <div className={styles["todo-app"]}>
+      <Header listCount={ this.state.list.length } />
+      <AddTask onAddItem={ this.handleAddListItem } />
+      <TodoList list={ this.state.list } onDeleteListItem={ this.handleDeleteListItem } />
+    </div>
+  );
+  }
+}
+
+export default TodoApp;
